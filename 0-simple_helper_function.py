f1124d045e5c157b161409eb68419df94b28c33c
anthonyjoyyy <13063@holbertonstudents.com>
2026-08-23 06:39:37 +0000

    indexing

---
+++ 0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module containing a function that takes two integer arguments.
+"""
+
+
+def index_range(page, page_size):
+    "return the start and end indexes for pagination request"
+    start = (page - 1) * page_size
+    end = start + page_size
+    return [start:end]
