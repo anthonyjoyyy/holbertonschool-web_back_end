352292ec491d534769f5659c102fff66d0f344d8
anthonyjoyyy <13063@holbertonstudents.com>
2026-08-27 02:45:21 +0000

    returning list topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 """
-
+Module contains a function returning a list.
 """
+
+
+def schools_by_topic(mongo_collection, topic):
+    "returns the list of school having a specific topic"
+
+    return list(mongo_collection.find({"topics": topic}))
