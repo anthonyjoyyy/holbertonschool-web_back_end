9dba328f71d5a9b72c92714b16f0c1fcefe7a244
anthonyjoyyy <13063@holbertonstudents.com>
2026-08-31 07:12:29 +0000

    fixing parameters

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,3 +1,5 @@
-export default class Classroom
-constructor(_maxStudentsSize)
-this._maxStudentsSize = maxStudentsSize
+export default class Classroom {
+constructor(maxStudentsSize) {
+this._maxStudentsSize = maxStudentsSize;
+}
+}
