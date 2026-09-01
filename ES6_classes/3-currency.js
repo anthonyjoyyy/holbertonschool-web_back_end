942f5fc3fde360b455e3a0bf5832a2a49f95c5d5
anthonyjoyyy <13063@holbertonstudents.com>
2026-09-01 02:02:17 +0000

    currency task

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,24 @@
+export default class Currency {
+	constructor(code, name) {
+	
+	this._code = code;
+	this._name = name;
+	}
+
+	get code() {
+		return this._code;
+	}
+	set code(val) {
+		return this._code = val;
+	}
+	get name() {
+		return this._name;
+	}
+	set name() {
+		return this._name = val;
+	}
+
+	displayFullCurrency() {
+		return this._name(this._code)
+	}
+}
