1a8d1dac8f5dd5bd88038b318b559caeda9bbe69
anthonyjoyyy <13063@holbertonstudents.com>
2026-09-15 03:09:42 +0000

    filter n map

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(ListStudents, city, newGrades) {
+	if (!Array.isArray(listStudents) || !Array.isArray(newGrades)) {
+    return [];
+  }
+	return listStudents
+    .filter((student) => student.location === city)
+    .map((student) => {
+      // Find matching grade object for this student
+      const gradeObj = newGrades.find((item) => item.studentId === student.id);
+
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
