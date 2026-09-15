e877529c349a43c621c8142ea2ed3682afdd7575
anthonyjoyyy <13063@holbertonstudents.com>
2026-09-15 02:48:31 +0000

    reduce - fixing parameters

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -2,5 +2,5 @@ export default function getStudentIdsSum(listStudents) {
 	if (!Array.isArray(listStudents)) {
   return 0;
 }
-return listStudents.reduce((student, sum) => sum + student.id), 0;
+return listStudents.reduce((sum, student) => sum + student.id, 0);
 }
